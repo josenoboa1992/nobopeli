7250c7d0c7aae6207b9a14371baf96f86a94291d
Jose Noboa <josenoboa@gmail.com>
2025-12-13 14:46:41 -0400

    Refactor movie detail handling and enhance movie screen with loading state management

--- lib/presentation/providers/movies/movie_info_provider.dart
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -1,9 +1,25 @@
 import 'package:flutter_riverpod/legacy.dart';
+import 'package:nobopeli/domain/entities/movie.dart';
+import 'package:nobopeli/presentation/providers/movies/movies_repository_provider.dart';
 
-class MovieMapNotifier extends StateNotifier {
-  MovieMapNotifier() : super({});
+final movieInfoProvider =
+    StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) {
+      final movieRepository = ref.watch(movieRepositoryProvider);
+
+      return MovieMapNotifier(movieRepository.getMovieID);
+    });
+
+typedef GetMovieCallBack = Future<Movie> Function(String movieID);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallBack getMovie;
+  MovieMapNotifier(this.getMovie) : super({});
 
   Future<void> loadMovie(String movieId) async {
     if (state[movieId] != null) return;
+
+    final movie = await getMovie(movieId);
+
+    state = {...state, movieId: movie};
   }
 }
--- lib/presentation/screen/movies/movie_screen.dart
+++ lib/presentation/screen/movies/movie_screen.dart
@@ -1,23 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nobopeli/domain/entities/movie.dart';
+import 'package:nobopeli/presentation/providers/movies/movie_info_provider.dart';
 
-class MovieScreen extends StatefulWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String moviedID;
 
   const MovieScreen({super.key, required this.moviedID});
 
   @override
-  State<MovieScreen> createState() => _MovieScreenState();
+  ConsumerState<MovieScreen> createState() => _MovieScreenState();
 }
 
-class _MovieScreenState extends State<MovieScreen> {
+class _MovieScreenState extends ConsumerState<MovieScreen> {
   @override
   void initState() {
     super.initState();
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.moviedID);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Movied:${widget.moviedID}')));
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.moviedID];
+    if (movie == null) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          _CustomSliversAppBar(movie),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDetail(movie: movie),
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MovieDetail extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetail({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(movie.posterPath, width: size.width * 0.3),
+              ),
+              SizedBox(width: 15),
+
+              //descripcion
+              Expanded(
+                child: SizedBox(
+                  width: (size.width - 40) * 0.7,
+                  child: Column(
+                    children: [
+                      Text(
+                        movie.title,
+                        style: textStyle.titleLarge,
+                        textAlign: TextAlign.start,
+                      ),
+                      SizedBox(height: 10),
+                      Text(movie.overview),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 100),
+      ],
+    );
+  }
+}
+
+class _CustomSliversAppBar extends StatelessWidget {
+  final Movie movie;
+
+  const _CustomSliversAppBar(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.only(bottom: 10, left: 10, right: 10),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 16, color: Colors.white),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(
+          children: [
+            SizedBox(
+              width: size.width,
+              height: size.height * 0.7,
+              child: Image.network(movie.posterPath, fit: BoxFit.cover),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.transparent, Colors.black87],
+                    stops: [0.5, 1.0],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    colors: [Colors.black87, Colors.transparent],
+                    stops: [0.0, 0.3],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
